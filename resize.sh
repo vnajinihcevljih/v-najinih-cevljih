6d7f4b091f3da4267d7f3c2f7c54fd4af7c95b0f
Ana Godec <ana.godec5@gmail.com>
2019-11-10 11:58:14 +0100

    Jesen na Dunaju - new blogpost

--- resize.sh
+++ resize.sh
@@ -1,7 +1,7 @@
 set -o errexit
-mkdir resized
-for i in *.jpg;
+mkdir $1/resized
+for i in $1/*.jpg;
 do
 	echo $i;
-    convert -resize x900 -quality 60 "$i" "resized/$i";
+    convert -resize x900 -quality 60 "$i" "$1/resized/$(basename $i)";
 done;
